d61719fcfbc4b37468d8bfb24b922438e93d684a
Leo Tumwattana <leo.tumwattana@gmail.com>
2022-04-23 20:34:17 +0800

    Initial Commit

---
+++ TK2TextView/TK2TextViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TK2TextViewApp.swift
+//  TK2TextView
+//
+//  Created by Leo Tumwattana on 23/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct TK2TextViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
